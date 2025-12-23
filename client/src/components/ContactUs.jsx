116cef8d272fc8c97acc397e6bea0d5d42359fe8
Hasbyseptian12 <hasbyseptian4@gmail.com>
2025-12-23 21:30:43 +0700

    update footer

--- client/src/components/ContactUs.jsx
+++ client/src/components/ContactUs.jsx
@@ -171,50 +171,6 @@ const ContactUs = () => {
 
             <footer className="footer">
                 <div className="container">
-                    <div className="footer-grid">
-                        <div className="footer-column">
-                            <h4>Relasi Investor</h4>
-                            <ul>
-                                <li>Laporan Tahunan</li>
-                                <li>Laporan Keuangan</li>
-                                <li>Tata Kelola Perusahaan</li>
-                            </ul>
-                        </div>
-                        <div className="footer-column">
-                            <h4>Keberlanjutan</h4>
-                            <ul>
-                                <li>Aktivitas CSR</li>
-                                <li>Aktivitas SHE</li>
-                            </ul>
-                        </div>
-                        <div className="footer-column">
-                            <h4>Produk & Layanan</h4>
-                            <ul>
-                                <li>WIKA Realty Archipelago</li>
-                                <li>Pengembangan Properti</li>
-                                <li>Asset & Investasi Properti</li>
-                                <li>Hotel</li>
-                            </ul>
-                        </div>
-                        <div className="footer-column">
-                            <h4>Berita & Acara</h4>
-                            <ul>
-                                <li>Media</li>
-                                <li>E-Magazine</li>
-                                <li>Promo</li>
-                            </ul>
-                        </div>
-                        <div className="footer-column">
-                            <h4>Perusahaan</h4>
-                            <ul>
-                                <li>Tentang Kami</li>
-                                <li>Manajemen Perusahaan</li>
-                                <li>Sertifikat & Penghargaan</li>
-                                <li>Karir</li>
-                                <li>Hubungi Kami</li>
-                            </ul>
-                        </div>
-                    </div>
                     <div className="footer-bottom">
                         <p>Copyright © 2025 Fachri Property Group</p>
                     </div>
