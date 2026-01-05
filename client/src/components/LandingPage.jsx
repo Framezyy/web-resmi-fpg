a2388db96c98313b590b4fc04a7f5e5f7c771eed
Ichsan <muhammadichsan2017@gmail.com>
2026-01-05 11:18:40 +0700

    gambar background

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LandingPage.css';
 import heroBgsatu from '../assets/images/image.png';
-import heroBgdua from '../assets/images/homedua.png';
-import heroBgtiga from '../assets/images/hometiga.png';
-import heroBgempat from '../assets/images/homeempat.png';
+import heroBgdua from '../assets/images/homesatu.png';
+import heroBgtiga from '../assets/images/homedua.png';
+import heroBgempat from '../assets/images/hometiga.png';
 import logoHotampt from '../assets/images/logoitampt.png'; // ADD
 
 const LandingPage = () => {
